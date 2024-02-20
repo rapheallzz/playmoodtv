17a350d4b3a2378714ec68973e2cbf5e80cc4440
rapheallzz <70053913+rapheallzz@users.noreply.github.com>
2024-02-20 13:41:44 +0100

    Delete src directory

--- src/features/useAuth.js
+++
@@ -1,9 +0,0 @@
-// // useAuth.js
-// import { useSelector } from 'react-redux';
-
-// const useAuth = () => {
-//   const { user } = useSelector((state) => state.auth);
-//   return user;
-// };
-
-// export default useAuth;
